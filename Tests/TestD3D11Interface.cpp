28a756a95d2268fb478186b2ae8866ea00ad91b4
SleepyEthan <Ethan2015D@gmail.com>
2021-06-27 22:51:13 -0500

    Added Test file for D3D11 Interface, I don't think this needs tested.

---
+++ Tests/TestD3D11Interface.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../SleepyEngine/D3D11Interface.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestD3D11Interface
+{
+	TEST_CLASS( TesD3D11Interface )
+	{
+	public:
+		TEST_METHOD( Ensure_D3D11Interface_Creation_Succeeds )
+		{
+		}
+	};
+}
