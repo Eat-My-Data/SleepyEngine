e61648c1663e7d729f08b216114e6078351be4fd
SleepyEthan <Ethan2015D@gmail.com>
2021-06-27 20:03:52 -0500

    Added test file for Win32Window, not sure what tests should be included yet.

---
+++ Tests/TestWin32Window.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../SleepyEngine/Win32Window.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestWin32Window
+{
+	TEST_CLASS( TestWin32Window )
+	{
+	public:
+		TEST_METHOD( Ensure_TestWin32Window_Creation_Succeeds )
+		{
+		}
+	};
+}
