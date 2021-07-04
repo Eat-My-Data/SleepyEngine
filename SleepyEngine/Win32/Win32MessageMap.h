67e341c20487f1e412c39c2df94508f26a096a81
SleepyEthan <Ethan2015D@gmail.com>
2021-07-03 21:17:45 -0500

    Fixed ignored files.

---
+++ SleepyEngine/Win32/Win32MessageMap.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <unordered_map>
+#include <Windows.h>
+
+class Win32MessageMap
+{
+public:
+	Win32MessageMap() noexcept;
+	std::string operator()( DWORD msg, LPARAM lp, WPARAM wp ) const noexcept;
+private:
+	std::unordered_map<DWORD, std::string> map;
+};
