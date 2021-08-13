6bed894d62edc791e68b1c3ffe781ba53c0cf1c0
SleepyEthan <Ethan2015D@gmail.com>
2021-08-13 14:53:59 -0500

    Copied over rastertek font engine code: http://www.rastertek.com/dx11tut12.html

--- SleepyEngine/Shaders/PixelShader.hlsl
+++
@@ -1,9 +0,0 @@
-float4 main(float4 pos : Position, float3 normal : NORMAL) : SV_TARGET
-{ 
-    float4 output;
-    
-    float3 lightDirection = { -1.0f, -1.0f, -1.0f };
-    output = float4(max(dot(normalize(lightDirection.xyz), normalize(normal)), 0.01f ), 0.0f, 0.0f, 1.0f);
-    
-    return output; //float4(1.0f, 0.0f, 0.0f, 1.0f);
-}
