e4feca15917bb6745a09b1787274c84d5e888926
SleepyEthan <Ethan2015D@gmail.com>
2021-08-16 17:55:44 -0500

    Revert "Revert "Removed tutorial files, going to try my own way.""
    This reverts commit 62dd576a6d81d755b98790d6db9930807558234c.

--- SleepyEngine/GUI/ChooseRenderTechnique.cpp
+++ SleepyEngine/GUI/ChooseRenderTechnique.cpp
@@ -39,15 +39,15 @@ void ChooseRenderTechnique::Draw( GraphicsDeviceInterface& gdi )
 	auto model = Rectangle::Make( (f32)m_iX - 640.0f, (f32)m_iY + 360.0f - (f32)m_iHeight, (f32)m_iWidth, (f32)m_iHeight );
 
 	VertexBuffer::VertexBuffer( gdi, model.m_VBVertices ).Bind( gdi );
-	auto pvs = VertexShader::Resolve( gdi, "../SleepyEngine/Shaders/Bin/PhongVS.cso" );
+	auto pvs = VertexShader::Resolve( gdi, "./Shaders/Bin/PhongVS.cso" );
 	pvs->Bind( gdi );
 	auto pvsbc = pvs->GetBytecode();
-	PixelShader::PixelShader( gdi, "../SleepyEngine/Shaders/Bin/PhongPS.cso" ).Bind( gdi );
+	PixelShader::PixelShader( gdi, "./Shaders/Bin/PhongPS.cso" ).Bind( gdi );
 	Bind::PixelConstantBuffer<ChooseRenderTechnique::ColorConst>::PixelConstantBuffer( gdi, color, 1u ).Bind( gdi );
 	InputLayout::InputLayout( gdi, model.m_VBVertices.GetLayout(), pvsbc ).Bind( gdi );
 	Topology::Topology( gdi, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST ).Bind( gdi );
 	Rasterizer::Rasterizer( gdi, true ).Bind( gdi );
-
+	Texture::Texture( gdi, "./GUI/Text/font_01.jpg",2u ).Bind( gdi );
 	gdi.GetContext()->Draw( 6u, 0u );
 }
 
