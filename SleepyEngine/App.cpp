5629e7938775935f4622eec1440ecdcf3efd8544
SleepyEthan <Ethan2015D@gmail.com>
2022-02-28 21:24:00 -0600

    Added initial files for deferred render graph.

--- SleepyEngine/App.cpp
+++ SleepyEngine/App.cpp
@@ -12,7 +12,7 @@
 App::App( const std::string& commandLine )
 	:
 	commandLine( commandLine ),
-	wnd( 1280, 720, "Sleepy Engine" ),
+	wnd( 1280*3/2, 720*3/2, "Sleepy Engine" ),
 	scriptCommander( TokenizeQuoted( commandLine ) ),
 	light( wnd.Gfx(), { 10.0f,5.0f,0.0f } ) 
 {
